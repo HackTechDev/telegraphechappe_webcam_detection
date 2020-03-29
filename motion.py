e5898890ffd97d343259b16b768dc56a9c452181
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-03-29 21:14:25 +0200

    Update

--- motion.py
+++ motion.py
@@ -16,23 +16,22 @@ motionCounter = 0
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
+
 print(frame1.shape)
 while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)
+    dilated = cv2.dilate(thresh, None, iterations = 3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    secondBegin = time.time()
 
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
 
         if cv2.contourArea(contour) < 900:
             continue
-        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
         #print("Log: Move detected " + str(contour))
         print("Log: Move detected ")
@@ -66,12 +65,12 @@ while cap.isOpened():
             for pt in zip(*loc[::-1]):
                 if pt is not None:
                     flag = True
-                    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+                    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
                     break
 
             if flag == True:
                 print("log: " + f)
-                cv2.imwrite('detected.jpg',img_rgb)
+                cv2.imwrite('detected.jpg', img_rgb)
                 signal_dict = {"signal": f}
                 #path_json = '/home/pi/public_html/telegraphechappe_web/signal.json'
                 path_json = 'signal.json'
